db09340cc5b58a1bea922b5cfd1d4108387ab40d
Jitse-Jan <jitsejan@gmail.com>
2019-02-18 11:48:53 +0000

    Rename HelloWorld to App

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import App from '@/components/App'
 import Characters from '@/components/Characters'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'App',
+      component: App
     },
     {
       path: '/characters',
